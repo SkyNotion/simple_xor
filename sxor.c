6c65010e57628ee2cd657783592e409feacc62d9
SkyNotion <meshdrew6@gmail.com>
2026-01-15 16:19:31 +0100

    Huge speedup, xor's data per 8 byte block instead of 1 block used before
    the buffer is casted to a 64bit unsigned int pointer (`uint64_t`), this allows it to operate on 8 bytes at once (bascically xoring registers on a 64bit processor).

--- sxor.c
+++ sxor.c
@@ -19,39 +19,52 @@ const char help_message[] = "Usage: sxor [options] file1.dat file2.txt ...\n"
     blob - filedata
 */
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE (256 * 1024)
 
-/* It can be binary, i hate dealing with signed char */
-uint8_t* xor(uint8_t* key, size_t key_len, uint8_t* data, size_t len){
-    uint64_t key_index = 0;
-    for(int i = 0;i < len;i++){
-        data[i] ^= key[(key_index = key_index % key_len)];
+void xor(uint8_t* key, size_t key_len, uint8_t* data, size_t len, uint8_t* out){
+    uint64_t* data64 = (uint64_t*)data;
+    uint64_t* key64 = (uint64_t*)key;
+    uint64_t* out64 = (uint64_t*)out;
+
+    size_t itr = len / sizeof(uint64_t);
+    size_t rem = len % sizeof(uint64_t);
+    size_t i, r;
+
+    for(i = 0;i < itr;i++){
+        out64[i] = data64[i] ^ key64[0];
+    }
+
+    uint8_t key_index = 0;
+    for(r = (len - rem);r < len;r++){
+        out[r] = data[r] ^ key[(key_index = key_index % 8)];
         key_index++;
     }
-    return data;
 }
 
 void run_conversion(FILE* read_f, FILE* write_f, uint8_t* key){
+    size_t key_len = strlen(key);
+    int rfd = fileno(read_f);
+    int wfd = fileno(write_f);
     uint8_t buf[BUFFER_SIZE];
-    size_t buf_sz;
+    uint8_t out[BUFFER_SIZE];
+    ssize_t buf_sz;
     do{
-        buf_sz = fread(buf, 1, BUFFER_SIZE, read_f);
-        fwrite(xor(key, strlen((char*)key), buf, buf_sz), 1, buf_sz, write_f);
+        buf_sz = read(rfd, buf, BUFFER_SIZE);
+        xor(key, key_len, buf, buf_sz, out);
+        write(wfd, out, buf_sz);
     }while(buf_sz != 0);
     fclose(read_f);
     fclose(write_f);
 }
 
-int encode_file(char* fname, char* key){
+int encode_file(char* fname, uint8_t* key){
     FILE* read_f = fopen(fname, "rb");
     if(read_f == NULL){
         fprintf(stderr, "Failed to open file for reading\n");
         return 1;
     }
 
-    char buf[1024], ofname[64];
-
-    /* Hey it works */
+    char ofname[64];
     struct timeval tv;
     gettimeofday(&tv, NULL);
     srand(time(NULL));
@@ -63,11 +76,24 @@ int encode_file(char* fname, char* key){
         return 1;
     }
 
-    uint32_t fname_sz = (uint32_t)strlen(fname);
-    memcpy(buf, &fname_sz, sizeof(fname_sz));
-    fwrite(xor((uint8_t*)key, strlen(key), (uint8_t*)buf, sizeof(fname_sz)), 1, sizeof(fname_sz), write_f);
-    memcpy(&buf, fname, fname_sz);
-    fwrite(xor((uint8_t*)key, strlen(key), (uint8_t*)buf, fname_sz), 1, fname_sz, write_f);
+    uint32_t name_len = (uint32_t)strlen(fname);
+    uint8_t* buf = (uint8_t*)malloc(1024);
+    uint8_t* out = (uint8_t*)malloc(1024);
+
+    memcpy(buf, &name_len, sizeof(uint32_t));
+    xor(key, strlen(key), buf, sizeof(uint32_t), out);
+    fwrite(out, 1, sizeof(uint32_t), write_f);
+
+    buf = (uint8_t*)realloc(buf, name_len);
+    out = (uint8_t*)realloc(out, name_len);
+
+    memcpy(buf, fname, name_len);
+    xor(key, strlen(key), buf, name_len, out);
+    fwrite(out, 1, name_len, write_f);
+
+    free(buf);
+    free(out);
+    fflush(write_f);
 
     run_conversion(read_f, write_f, key);
 }
@@ -78,22 +104,31 @@ int decode_file(char* fname, char* key){
         fprintf(stderr, "Failed to open file for reading\n");
         return 1;
     }
-
-    uint8_t buf[1024];
-    char ofname[64];
-
-    uint32_t name_len;
+ 
+    uint32_t name_len = 0;
+    uint8_t* buf = (uint8_t*)malloc(1024);
+    uint8_t* out = (uint8_t*)malloc(1024);
+    
     fread(buf, 1, sizeof(uint32_t), read_f);
-    memcpy(&name_len, xor((uint8_t*)key, strlen(key), buf, sizeof(uint32_t)), sizeof(uint32_t));
-    fread(ofname, 1, name_len, read_f);
-    xor((uint8_t*)key, strlen(key), ofname, name_len);
-    ofname[name_len] = '\0';
+    xor(key, strlen(key), buf, sizeof(uint32_t), out);
+    memcpy(&name_len, out, sizeof(uint32_t));
 
-    FILE* write_f = fopen(ofname, "wb");
+    buf = (uint8_t*)realloc(buf, name_len + 1);
+    out = (uint8_t*)realloc(out, name_len + 1);
+
+    fread(buf, 1, name_len, read_f);
+    xor(key, strlen(key), buf, name_len, out);
+    out[name_len] = 0x00;
+
+    FILE* write_f = fopen(out, "wb");
     if(write_f == NULL){
         fprintf(stderr, "Failed to open file for writing\n");
         return 1;
     }
+
+    free(buf);
+    free(out);
+    fflush(read_f);
 
     run_conversion(read_f, write_f, key);
 }
@@ -131,8 +166,9 @@ int main(int argc, char* argv[]){
         return -1;
     }
 
-    if(strlen(key) > 4096){
-        fprintf(stderr, "Key is too large\n");
+    if(strlen(key) != 8){
+    //if(strlen(key) > 4096){
+        fprintf(stderr, "Key must be 8 characters\n");
         fprintf(stderr, help_message);
         return -1;
     }
@@ -140,9 +176,9 @@ int main(int argc, char* argv[]){
     if(optind < argc){
         while(optind < argc){
             if(encode){
-                encode_file(argv[optind++], key);
+                encode_file(argv[optind++], (uint8_t*)key);
             }else{
-                decode_file(argv[optind++], key);
+                decode_file(argv[optind++], (uint8_t*)key);
             }
         }
     }
